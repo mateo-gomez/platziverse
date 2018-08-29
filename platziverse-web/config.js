29e5f75a2976a21b39843f21474ac7dbb25d73b0
Mateo Gómez <mateogomezmolina@gmail.com>
2018-08-28 22:53:20 -0500

    arreglando problemas

--- platziverse-web/config.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-module.exports = {
-  endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
-  apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBsYXR6aSIsImFkbWluIjp0cnVlLCJwZXJtaXNzaW9ucyI6WyJtZXRyaWNzOnJlYWQiXSwiaWF0IjoxNTM1NTExNzA4fQ.LxpxfHCnm6ocQ7oBaCb67SFYBkQZhhhlORs6o7LF69g'
-}
--- platziverse-web/server.js
+++ platziverse-web/server.js
@@ -8,7 +8,6 @@ const socketio = require('socket.io');
 const chalk = require('chalk');
 const PlatziverseAgent = require('platziverse-agent');
 
-const proxy = require('./proxy');
 const { pipe } = require('./utils')
 
 const port = process.env.PORT || 8080
@@ -20,23 +19,11 @@ const agent = new PlatziverseAgent()
 app.use(express.static(path.join(__dirname, 'public')))
 // ./public - ./ + public
 
-app.use('/', proxy)
-
 // Socket.io / WebSockets
 io.on('connect', socket => {
   debug(`Connected ${socket.id}`)
 
   pipe(agent, socket)
-})
-
-// Express Error Handler
-app.use((err, req, res, next) => {
-  debug(`Error: ${err.message}`)
-
-  if (err.message.match(/not found/)) {
-    return res.status(404).send({ error: err.message })
-  }
-  res.status(500).send({ error: err.message })
 })
 
 function handleFatalError(err) {
