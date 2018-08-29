8bb2d7a7db7a4f75f48f80f2cf57f7345e81e3b5
Mateo Gómez <mateogomezmolina@gmail.com>
2018-08-28 21:46:18 -0500

    creando comunicacion entre modulos

---
+++ platziverse-web/server.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const debug = require('debug')('platziverse:web')
+const http = require('http')
+const path = require('path');
+const express = require('express')
+const socketio = require('socket.io');
+const chalk = require('chalk');
+const PlatziverseAgent = require('platziverse-agent');
+
+const { pipe } = require('./utils')
+
+const port = process.env.PORT || 8080
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+const agent = new PlatziverseAgent()
+
+app.use(express.static(path.join(__dirname, 'public')))
+// ./public - ./ + public
+
+// Socket.io / WebSockets
+io.on('connect', socket => {
+  debug(`Connected ${socket.id}`)
+
+  pipe(agent, socket)
+})
+
+function handleFatalError(err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
+
+server.listen(port, () => {
+  console.log(`${chalk.green('[platziverse-web]')} server listening on port ${port}`)
+  agent.connect()
+})
